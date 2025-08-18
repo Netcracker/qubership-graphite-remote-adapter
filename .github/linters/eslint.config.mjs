b674818e266898a44e56da540d91d88acf0b7409
IldarMinaev <ildar.minaev.qubership@gmail.com>
2025-08-19 00:20:06 +0400

    chore: fix linter founds

--- .github/linters/eslint.config.mjs
+++ .github/linters/eslint.config.mjs
@@ -1,3 +1,5 @@
+// See for details:https://eslint.org/docs/latest/use/configure/configuration-files#configuration-file
+
 export default [
 	{
 		// Note: there should be no other properties in this object
