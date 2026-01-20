499b86126a80dd2ff6f955624a100ce03654b7e9
Beauline <valentina.feshina.qubership@gmail.com>
2026-01-20 11:08:09 +0300

    chore: fix linting issues and correct license notice

--- client/graphite/test/write_test.go
+++ client/graphite/test/write_test.go
@@ -1,4 +1,6 @@
-// Copyright 2024-2025 NetCracker Technology Corporation
+//go:build cgo
+
+// Copyright NetCracker Technology Corporation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,7 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 
 package test
 
